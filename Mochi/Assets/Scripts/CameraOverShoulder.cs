d0a96ce0a534b10de1f5c1e7e8e54e19a93f7762
molenubar <molenubar@gmail.com>
2021-01-28 23:16:55 -0800

    simple sphere scene to play around with

---
+++ Mochi/Assets/Scripts/CameraOverShoulder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOverShoulder : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset = new Vector3() { x = 0, y = 1, z = -2 };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Mochi/Assets/Scripts/SpherePlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherePlayerController : MonoBehaviour
+{
+    public float Accellaration = 100;
+    public float moveVert = 0;
+    public float moveHor = 0;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+        void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        moveVert = Input.GetAxis("Vertical");
+        moveHor = Input.GetAxis("Horizontal");
+
+        movement = new Vector3(moveHor, 0, moveVert);
+        rb.AddForce(movement * Accellaration);
+        
+    }
+}
