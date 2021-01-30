bd652e925db461b2e430826d3e7efb5135ea4a23
Zach <danzzach@gmail.com>
2021-01-29 20:26:02 -0800

    fix prefab, WIP dialogue

---
+++ Mochi/Assets/Scripts/Classes/DialogueEncounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+public class DialogueEncounter 
+{
+    public static string DialougeEncountersTag = @"Encounters";
+    
+    public string Tag { get; private set; }
+
+    public List<Tuple<string, string>> Dialogues { get; private set; }
+
+    public int TimesRead { get; private set; }
+
+    public DialogueEncounter()
+    {
+        this.Tag = "[none]";
+        this.Dialogues = new List<Tuple<string, string>>();
+        this.TimesRead = 0;
+    }
+
+    public void ReadXML(XmlNode node)
+    {
+        this.Tag = node.Attributes[nameof(this.Tag)].Value;
+
+        foreach (var dialogueNodes in node.ChildNodes)
+        {
+
+        }
+    }
+}
---
+++ Mochi/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Unity.Collections;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public string ResourcesRelativeDialogueXMLFilePath;
+
+    [ReadOnly]
+    public string DialogueXMLFilePath;
+
+    [ReadOnly]
+    public List<string> AvailableTags;
+
+    [ReadOnly]
+    public Dictionary<string, DialogueEncounter> TagsToEncounters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(string.IsNullOrEmpty(this.ResourcesRelativeDialogueXMLFilePath), $"{nameof(this.ResourcesRelativeDialogueXMLFilePath)} cannot be null or empty. Please provide one.");
+        this.AvailableTags = new List<string>();
+        this.TagsToEncounters = new Dictionary<string, DialogueEncounter>();
+
+
+        var text = (TextAsset)Resources.Load(this.ResourcesRelativeDialogueXMLFilePath);
+        var xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(text.text);
+
+        var encounters = xmlDoc.SelectSingleNode($@"//{DialogueEncounter.DialougeEncountersTag}");
+
+        foreach (XmlNode encounterXml in encounters.ChildNodes)
+        {
+            var encounter = new DialogueEncounter();
+            encounter.ReadXML(encounterXml);
+
+            Debug.Assert(TagsToEncounters.TryGetValue(encounter.Tag, out _), $"All Tags must be unique. Tag {encounter.Tag} is already taken.");
+
+            this.AvailableTags.Add(encounter.Tag);
+            this.TagsToEncounters.Add(encounter.Tag, encounter);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
