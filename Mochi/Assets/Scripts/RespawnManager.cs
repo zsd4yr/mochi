31f2cecbd90dcf9f1f28b1a5287b9e13db01662b
Zach <danzzach@gmail.com>
2021-01-30 10:50:52 -0800

    wip respawn controller work

---
+++ Mochi/Assets/Scripts/RespawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RespawnManager : MonoBehaviour
+{
+    [ReadOnly]
+    public List<RespawnPlatform> RespawnPlatforms;
+
+    [ReadOnly]
+    public RespawnPlatform CurrentRespawnPlatform;
+
+    void Start()
+    {
+        this.RespawnPlatforms = new List<RespawnPlatform>();
+        this.RespawnPlatforms.AddRange(GameObject.FindGameObjectsWithTag(RespawnPlatform.RespawnPlatformTag)
+            .ToList()
+            .Select(each => each.GetComponent<RespawnPlatform>()));
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.rKey.wasReleasedThisFrame)
+        {
+            this.RespawnRobot();
+        }
+    }
+
+    private void RespawnRobot()
+    {
+
+    }
+}
---
+++ Mochi/Assets/Scripts/RespawnPlatform.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class RespawnPlatform : MonoBehaviour
+{
+    public static string RespawnPlatformTag = "RespawnPlatform";
+}
