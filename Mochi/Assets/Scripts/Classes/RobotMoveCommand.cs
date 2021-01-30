bb3c57c4d0bbb7fa2970c044593c59aa0be519b8
Zach <danzzach@gmail.com>
2021-01-30 14:40:27 -0800

    text mesh pro, more robot stuff, respawning, robot input screen

---
+++ Mochi/Assets/Scripts/Classes/RobotMoveCommand.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RobotMoveCommand : IRobotCommand
+{
+    public Vector3 WorldPosition { get; private set; }
+    public Vector3 ScreenPosition { get; private set; }
+
+    public RobotMoveCommand(Vector3 world, Vector3 screen)
+    {
+        WorldPosition = world;
+        ScreenPosition = screen;
+    }
+}
--- Mochi/Assets/Scripts/UIManager.cs
+++ Mochi/Assets/Scripts/UIManager.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public Canvas RobotInputScreen;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Assert(this.RobotInputScreen != null, $"{nameof(this.RobotInputScreen)} cannot be null upon start. Please hook it up to {this.gameObject.name}");
     }
 
     // Update is called once per frame
