7892b773838311ec1c13d69f48406637674e0b2d
molenubar <molenubar@gmail.com>
2021-01-31 01:32:47 -0800

    player controller updated for rotation

--- Mochi/Assets/Scripts/PlayerController.cs
+++ Mochi/Assets/Scripts/PlayerController.cs
@@ -3,11 +3,15 @@
 public class PlayerController : MonoBehaviour
 {
     public static string PlayerTag = "Player";
-
+    public GameObject visual;
     Rigidbody rb;
     GameControls controls;
     public float Accellaration = 100f;
     public Vector2 move;
+    private Vector3 prevPos;
+
+    private float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
 
     private void Awake()
     {
@@ -36,8 +40,18 @@ public class PlayerController : MonoBehaviour
         controls.GameController.Movement.canceled += ctx => move = Vector2.zero;
 
         Vector3 movement = new Vector3(move.x, 0, move.y);
+        Vector3 movementNomral = new Vector3(move.x, 0, move.y).normalized;
         rb.AddForce(movement * Accellaration);
-
     }
 
+    private void LateUpdate()
+    {
+        if (rb.velocity.magnitude > 0.1f)
+        {
+
+            float targetAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(visual.transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            visual.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+    }
 }
