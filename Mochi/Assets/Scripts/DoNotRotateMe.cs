d886d66031eee7c9aa16eef2d3471e3a545d6583
Zach <danzzach@gmail.com>
2021-01-31 09:48:17 -0800

    I can figure out how to not rotate the camera under the player just fine thank you

---
+++ Mochi/Assets/Scripts/DoNotRotateMe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotRotateMe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.eulerAngles = Vector3.zero;
+        this.gameObject.transform.localEulerAngles = Vector3.zero;
+    }
+}
