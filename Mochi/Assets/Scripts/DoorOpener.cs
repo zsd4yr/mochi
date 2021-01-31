bc75b7f4132a4b3da6021e7e392de145c9abae92
molenubar <molenubar@gmail.com>
2021-01-31 14:42:31 -0800

    door prefab that opens & closes

---
+++ Mochi/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    [ShowOnly]
+    public Collider collider;
+    public Animator animator;
+
+    public bool openTrigger = false;
+
+    public bool closeTrigger = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = gameObject.GetComponent<Collider>();
+        //animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Robot"))
+        {
+            if(openTrigger)
+            {
+                animator.SetBool("isOpening", true);
+            }
+            else if (closeTrigger)
+            {
+                animator.SetBool("isOpening", false);
+            }
+        }
+    }
+
+
+}
