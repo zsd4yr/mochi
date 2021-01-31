0706f53e0cd42fa00f1666400793394b4f9b256e
Fihra <fabrofabian@gmail.com>
2021-01-31 11:59:58 -0800

    testing fmod sfx objects

---
+++ Mochi/Assets/Scripts/HazardSFX.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class HazardSFX : MonoBehaviour
+{
+    Rigidbody rb;
+    //public string hazardEvent;
+    string parameterName = "Player_Distance";
+    private EventInstance instance;
+
+    [EventRef]
+    public string fmodEvent;
+
+    [SerializeField] [Range(0.00f, 1.00f)]
+    private float distance;
+
+    //EventInstance hazard;
+    //EventDescription hazardDistance;
+    //PARAMETER_DESCRIPTION triggerHazard;
+    //PARAMETER_ID hazardID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        instance = RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
+
+        //hazard = RuntimeManager.CreateInstance(hazardEvent);
+        //hazardDistance = RuntimeManager.GetEventDescription(hazardEvent);
+
+        //hazardEvent.start();
+        //hazard.setParameterByName(parameterName, 1.00f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        instance.setParameterByName("Player_Distance", distance);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            Debug.Log("Inside Hazard");
+            //hazardDistance.getParameterDescriptionByName("Player_Distance", out triggerHazard);
+            //hazardID = triggerHazard.id;
+
+            //hazard.setParameterByName(parameterName, 1.00f);
+            //hazard.setParameterByID(hazardID, 1.00f);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Outside Hazard");
+            //hazard.setParameterByName(parameterName, 0.00f);
+            //hazard.setParameterByID(hazardID, 0.00f);
+        }
+    }
+
+}
