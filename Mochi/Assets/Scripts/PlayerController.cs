f7513b18753e34383bd7660c55e24e1a931db6e8
GregRGM <g.morton@digipen.edu>
2021-03-11 07:06:54 -0800

    PlayerController movement updates.

--- Mochi/Assets/Scripts/PlayerController.cs
+++ Mochi/Assets/Scripts/PlayerController.cs
@@ -28,9 +28,9 @@ public class PlayerController : MonoBehaviour
     [ShowOnly]
     public Animator PlayerAnimator;
 
-    public float timeToRunFromWalkSeconds, visualTargetAngle, visualAngle;
+    public float timeToRunFromWalkSeconds;
 
-    private float timeSinceStartedWalking;
+    private float timeSinceStartedWalking, visualTargetAngle, visualAngle;
     public const string isWalking = nameof(isWalking);
     public const string isRunning = nameof(isRunning);
     public const string isJumping = nameof(isJumping);
@@ -176,6 +176,41 @@ public class PlayerController : MonoBehaviour
             float targetAngle = Mathf.Atan2(Rigidbody.velocity.x, Rigidbody.velocity.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(visual.transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             visual.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+
+        if (this.Rigidbody.velocity.magnitude >= 0.01)
+        {
+            this.timeSinceStartedWalking += Time.deltaTime;
+            //if (this.PlayerAnimator.GetBool(Crawling) == false)
+            //{
+
+            if (this.timeSinceStartedWalking >= this.timeToRunFromWalkSeconds)
+            {
+                this.PlayerAnimator.SetBool(isWalking, false);
+                this.PlayerAnimator.SetBool(isRunning, true);
+            }
+            else
+            {
+                this.PlayerAnimator.SetBool(isWalking, true);
+                this.PlayerAnimator.SetBool(isRunning, false);
+            }
+            //}                
+            //else 
+            //{
+            //this.PlayerAnimator.SetBool(Crawling, false);
+            //this.PlayerAnimator.SetBool(isCrawling, true);
+            //}
+        }
+        else
+        {
+            this.timeSinceStartedWalking = 0.0f;
+            this.PlayerAnimator.SetBool(isWalking, false);
+            this.PlayerAnimator.SetBool(isRunning, false);
+            this.PlayerAnimator.SetBool(isCrawling, false);
+            /*if (crawler)
+            {
+                this.PlayerAnimator.SetBool(Crawling, true);
+            }*/
         }
     }
 }
