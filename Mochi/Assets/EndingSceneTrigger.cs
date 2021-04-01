e8d16717980251aee7aa268f993a00b0cecff3bd
GregRGM <g.morton@digipen.edu>
2021-03-31 19:35:43 -0700

    Added Fade to Ending, Moved Input Feedback

--- Mochi/Assets/EndingSceneTrigger.cs
+++ Mochi/Assets/EndingSceneTrigger.cs
@@ -9,20 +9,26 @@ public class EndingSceneTrigger : MonoBehaviour
     // Start is called before the first frame update
     AsyncOperation operation;
     public string scene;
+    public int sceneNum = 2;
     public Image image;
     public float timeToLerp = 2f;
+
+    public FadeImageInOut FadeIO;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+
+        if (other.gameObject.CompareTag("Player"))
             StartCoroutine(BeginLoad(scene));
     }
 
     private IEnumerator BeginLoad(string sceneName)
     {
+        if (FadeIO != null)
+        { FadeIO.FadeIn(); }
+
         yield return new WaitForSeconds(timeToLerp);
-        FadeToImage();
+        SceneManager.LoadScene(2);
         yield return new WaitForSeconds(timeToLerp);
-        operation = SceneManager.LoadSceneAsync(sceneName);
         yield return null;
     }
 
---
+++ Mochi/Assets/Scripts/FadeImageInOut.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Image))]
+
+public class FadeImageInOut : MonoBehaviour {
+    Image img;
+    // Use this for initialization
+    Color imgColor;
+
+    public GameObject ImageObject;
+    public float m_FadeTime = 2f;
+	void Start () {
+		          
+        StartCoroutine(FadeInAndOut(ImageObject, false, 1f));
+	}
+    
+    public void FadeIn()
+    {
+        StartCoroutine(FadeInAndOut(ImageObject, true, m_FadeTime));
+    }
+
+    public IEnumerator FadeInAndOut(GameObject objectToFade, bool fadeIn, float duration)
+    {
+        float counter = 0f;
+
+        //Set Values depending on if fadeIn or fadeOut
+        float a, b;
+        if (fadeIn)
+        {
+            a = 0;
+            b = 1;
+        }
+        else
+        {
+            a = 1;
+            b = 0;
+        }
+
+        int mode = 0;
+        Color currentColor = Color.clear;
+
+        SpriteRenderer tempSPRenderer = objectToFade.GetComponent<SpriteRenderer>();
+        Image tempImage = objectToFade.GetComponent<Image>();
+        RawImage tempRawImage = objectToFade.GetComponent<RawImage>();
+        MeshRenderer tempRenderer = objectToFade.GetComponent<MeshRenderer>();
+        Text tempText = objectToFade.GetComponent<Text>();
+
+        //Check if this is a Sprite
+        if (tempSPRenderer != null)
+        {
+            currentColor = tempSPRenderer.color;
+            mode = 0;
+        }
+        //Check if Image
+        else if (tempImage != null)
+        {
+            currentColor = tempImage.color;
+            mode = 1;
+        }
+        //Check if RawImage
+        else if (tempRawImage != null)
+        {
+            currentColor = tempRawImage.color;
+            mode = 2;
+        }
+        //Check if Text 
+        else if (tempText != null)
+        {
+            currentColor = tempText.color;
+            mode = 3;
+        }
+
+        //Check if 3D Object
+        else if (tempRenderer != null)
+        {
+            currentColor = tempRenderer.material.color;
+            mode = 4;
+
+            //ENABLE FADE Mode on the material if not done already
+            tempRenderer.material.SetFloat("_Mode", 2);
+            tempRenderer.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            tempRenderer.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            tempRenderer.material.SetInt("_ZWrite", 0);
+            tempRenderer.material.DisableKeyword("_ALPHATEST_ON");
+            tempRenderer.material.EnableKeyword("_ALPHABLEND_ON");
+            tempRenderer.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            tempRenderer.material.renderQueue = 3000;
+        }
+        else
+        {
+            yield break;
+        }
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            float alpha = Mathf.Lerp(a, b, counter / duration);
+
+            switch (mode)
+            {
+                case 0:
+                    tempSPRenderer.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+                    break;
+                case 1:
+                    tempImage.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+                    break;
+                case 2:
+                    tempRawImage.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+                    break;
+                case 3:
+                    tempText.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+                    break;
+                case 4:
+                    tempRenderer.material.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+                    break;
+            }
+            yield return null;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
