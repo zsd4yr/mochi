ca780ef6aa3c709f06fea7695bbc4bb6a50a93a2
GregRGM <g.morton@digipen.edu>
2021-04-06 23:04:05 -0700

    Updated level, added EndGame commands to restart and quit game.

--- Mochi/Assets/EndGameCommands.cs
+++ Mochi/Assets/EndGameCommands.cs
@@ -7,23 +7,51 @@ using UnityEngine.SceneManagement;
 public class EndGameCommands : MonoBehaviour
 {
     public string SceneString = "GregScene2";
+    GameControls controls;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        controls = new GameControls();
+        controls.GameController.Quit.performed += QuitGame;
+        controls.GameController.Restart.performed += RestartGame;
     }
 
-    // Update is called once per frame
+    private void RestartGame(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        SceneManager.LoadScene(SceneString);
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+        controls.GameController.Quit.performed -= QuitGame;
+    }
+
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Escape))
+        //if(controls.GameController)
+        //{
+        //    Application.Quit();
+        //}
+        /*if(Input.GetKeyUp(KeyCode.Escape))
         {
             Application.Quit();
         }
         else if(Input.anyKeyDown)
         {
             SceneManager.LoadScene(SceneString);
-        }
+        }*/
+    }
+
+
+    private void QuitGame(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        Application.Quit();
     }
 }
--- Mochi/Assets/Scripts/GameControls.cs
+++ Mochi/Assets/Scripts/GameControls.cs
@@ -33,6 +33,38 @@ public class @GameControls : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Quit"",
+                    ""type"": ""Button"",
+                    ""id"": ""c129dc01-e2da-472c-8903-b0c15034699c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Restart"",
+                    ""type"": ""Button"",
+                    ""id"": ""ad4a09c9-c248-4f8e-836c-cb5b8bef19bd"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Quit2"",
+                    ""type"": ""Button"",
+                    ""id"": ""a01d663e-fdf1-4d9d-811f-88fc0ea584b5"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Quti"",
+                    ""type"": ""Button"",
+                    ""id"": ""5c700357-35da-4c6f-9b24-04940a72b5d5"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -170,12 +202,56 @@ public class @GameControls : IInputActionCollection, IDisposable
                 },
                 {
                     ""name"": """",
-                    ""id"": ""8c56052d-09a9-4196-b025-5a9804542fea"",
-                    ""path"": ""<Mouse>/leftButton"",
+                    ""id"": ""95e52722-b629-4511-8efc-794f24d7d370"",
+                    ""path"": ""<Keyboard>/e"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""Keyboard and mouse"",
                     ""action"": ""Interact"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""35d9303a-34fd-44a5-80e2-6c0b5e2934db"",
+                    ""path"": ""<Keyboard>/escape"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Quit"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""74cafd96-571d-41fd-9ce6-f72b4f7197e7"",
+                    ""path"": ""<Keyboard>/r"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Restart"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""93e1eea2-a090-445c-8af6-db55226e4353"",
+                    ""path"": ""<Keyboard>/escape"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Quit2"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""cb7b4003-a8a6-4fe4-9fd0-2cecbc78dbd8"",
+                    ""path"": ""<Keyboard>/escape"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Quti"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -216,6 +292,10 @@ public class @GameControls : IInputActionCollection, IDisposable
         m_GameController = asset.FindActionMap("GameController", throwIfNotFound: true);
         m_GameController_Movement = m_GameController.FindAction("Movement", throwIfNotFound: true);
         m_GameController_Interact = m_GameController.FindAction("Interact", throwIfNotFound: true);
+        m_GameController_Quit = m_GameController.FindAction("Quit", throwIfNotFound: true);
+        m_GameController_Restart = m_GameController.FindAction("Restart", throwIfNotFound: true);
+        m_GameController_Quit2 = m_GameController.FindAction("Quit2", throwIfNotFound: true);
+        m_GameController_Quti = m_GameController.FindAction("Quti", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -267,12 +347,20 @@ public class @GameControls : IInputActionCollection, IDisposable
     private IGameControllerActions m_GameControllerActionsCallbackInterface;
     private readonly InputAction m_GameController_Movement;
     private readonly InputAction m_GameController_Interact;
+    private readonly InputAction m_GameController_Quit;
+    private readonly InputAction m_GameController_Restart;
+    private readonly InputAction m_GameController_Quit2;
+    private readonly InputAction m_GameController_Quti;
     public struct GameControllerActions
     {
         private @GameControls m_Wrapper;
         public GameControllerActions(@GameControls wrapper) { m_Wrapper = wrapper; }
         public InputAction @Movement => m_Wrapper.m_GameController_Movement;
         public InputAction @Interact => m_Wrapper.m_GameController_Interact;
+        public InputAction @Quit => m_Wrapper.m_GameController_Quit;
+        public InputAction @Restart => m_Wrapper.m_GameController_Restart;
+        public InputAction @Quit2 => m_Wrapper.m_GameController_Quit2;
+        public InputAction @Quti => m_Wrapper.m_GameController_Quti;
         public InputActionMap Get() { return m_Wrapper.m_GameController; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -288,6 +376,18 @@ public class @GameControls : IInputActionCollection, IDisposable
                 @Interact.started -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnInteract;
                 @Interact.performed -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnInteract;
                 @Interact.canceled -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnInteract;
+                @Quit.started -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnQuit;
+                @Quit.performed -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnQuit;
+                @Quit.canceled -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnQuit;
+                @Restart.started -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnRestart;
+                @Restart.performed -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnRestart;
+                @Restart.canceled -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnRestart;
+                @Quit2.started -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnQuit2;
+                @Quit2.performed -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnQuit2;
+                @Quit2.canceled -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnQuit2;
+                @Quti.started -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnQuti;
+                @Quti.performed -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnQuti;
+                @Quti.canceled -= m_Wrapper.m_GameControllerActionsCallbackInterface.OnQuti;
             }
             m_Wrapper.m_GameControllerActionsCallbackInterface = instance;
             if (instance != null)
@@ -298,6 +398,18 @@ public class @GameControls : IInputActionCollection, IDisposable
                 @Interact.started += instance.OnInteract;
                 @Interact.performed += instance.OnInteract;
                 @Interact.canceled += instance.OnInteract;
+                @Quit.started += instance.OnQuit;
+                @Quit.performed += instance.OnQuit;
+                @Quit.canceled += instance.OnQuit;
+                @Restart.started += instance.OnRestart;
+                @Restart.performed += instance.OnRestart;
+                @Restart.canceled += instance.OnRestart;
+                @Quit2.started += instance.OnQuit2;
+                @Quit2.performed += instance.OnQuit2;
+                @Quit2.canceled += instance.OnQuit2;
+                @Quti.started += instance.OnQuti;
+                @Quti.performed += instance.OnQuti;
+                @Quti.canceled += instance.OnQuti;
             }
         }
     }
@@ -324,5 +436,9 @@ public class @GameControls : IInputActionCollection, IDisposable
     {
         void OnMovement(InputAction.CallbackContext context);
         void OnInteract(InputAction.CallbackContext context);
+        void OnQuit(InputAction.CallbackContext context);
+        void OnRestart(InputAction.CallbackContext context);
+        void OnQuit2(InputAction.CallbackContext context);
+        void OnQuti(InputAction.CallbackContext context);
     }
 }
